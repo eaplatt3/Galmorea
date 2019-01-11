8426e2d13bb06b9bba914a56f86f694743a78434
Earl Platt III <eaplatt3@gmail.com>
2019-01-11 11:15:34 -0500

    Enabled Email & Password Reg -Still needs to be tested

--- app/src/main/java/com/example/sickl/galmorea/GalmoreasLoginPage.java
+++ app/src/main/java/com/example/sickl/galmorea/GalmoreasLoginPage.java
@@ -1,21 +1,14 @@
 package com.example.sickl.galmorea;
 
-import android.content.Intent;
+
 import android.graphics.drawable.AnimationDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.TextView;
-
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.common.api.GoogleApi;
-
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
--- app/src/main/java/com/example/sickl/galmorea/GalmoreasRegPage.java
+++ app/src/main/java/com/example/sickl/galmorea/GalmoreasRegPage.java
@@ -5,12 +5,17 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -25,15 +30,19 @@ import butterknife.ButterKnife;
 
 public class GalmoreasRegPage extends AppCompatActivity {
 
-    @BindView(R.id.btn_submit) Button regBtn;
-    @BindView(R.id.firstname_reg) EditText firstNameReg;
-    @BindView(R.id.lastname_reg) EditText lastNameReg;
-    @BindView(R.id.dob_reg) EditText dobReg;
-    @BindView(R.id.email_reg) EditText emailReg;
-    @BindView(R.id.password_reg) EditText passwordReg;
-    @BindView(R.id.parentbox) RadioButton parentBox;
-    @BindView(R.id.childbox) RadioButton childBox;
-    DatabaseReference mRef;
+    @BindView(R.id.btn_submit)
+    Button regBtn;
+    @BindView(R.id.firstname_reg)
+    EditText firstNameReg;
+    @BindView(R.id.lastname_reg)
+    EditText lastNameReg;
+    @BindView(R.id.dob_reg)
+    EditText dobReg;
+    @BindView(R.id.email_reg)
+    EditText emailReg;
+    @BindView(R.id.password_reg)
+    EditText passwordReg;
+    FirebaseAuth mRef;
 
 
     @Override
@@ -42,41 +51,81 @@ public class GalmoreasRegPage extends AppCompatActivity {
         setContentView(R.layout.activity_galmoreas_reg_page);
         ButterKnife.bind(this);
 
-        mRef = FirebaseDatabase.getInstance().getReference("Accounts");
+        mRef = FirebaseAuth.getInstance();
 
         regBtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                String strFirstName = firstNameReg.getText().toString();
-                String strLastName = lastNameReg.getText().toString();
-                String strDob = dobReg.getText().toString();
-                String strEmail = emailReg.getText().toString();
-                String strPassword = passwordReg.getText().toString();
+            public void onClick(View v) {
+                String email = emailReg.getText().toString().trim();
+                String password = passwordReg.getText().toString().trim();
+                String firstName = firstNameReg.getText().toString().trim();
+                String lastName = lastNameReg.getText().toString().trim();
+                String dob = dobReg.getText().toString().trim();
 
-
-                if (childBox.isChecked()) {
-                    mRef.push().setValue(new Person(strFirstName, strLastName, strDob, strEmail, strPassword));
+                if(TextUtils.isEmpty(firstName)){
+                    Toast.makeText(getApplicationContext(), "Enter First Name",
+                            Toast.LENGTH_SHORT).show();
+                    return;
                 }
 
-                else if (parentBox.isChecked()) {
-                    String strParentAcc = "Parent";
-                    mRef.push().setValue(new Person(strFirstName, strLastName, strDob, strEmail,
-                            strPassword));
-                } else {
-                    Toast.makeText(getApplicationContext(), "No Account Type has been" +
-                            " selected", Toast.LENGTH_SHORT).show();
+                if(TextUtils.isEmpty(lastName)){
+                    Toast.makeText(getApplicationContext(), "Enter Last Name",
+                            Toast.LENGTH_SHORT).show();
+                    return;
                 }
 
-                Intent startActivity = new Intent(view.getContext(), GalmoreasLoginPage.class);
-                startActivity(startActivity);
+                if(TextUtils.isEmpty(dob)){
+                    Toast.makeText(getApplicationContext(), "Enter Birthday",
+                            Toast.LENGTH_SHORT).show();
+                    return;
+                }
 
+                if(TextUtils.isEmpty(email)){
+                    Toast.makeText(getApplicationContext(), "Enter Email Address",
+                            Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (TextUtils.isEmpty(password)) {
+                    Toast.makeText(getApplicationContext(), "Enter Password", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (password.length() < 8) {
+                    Toast.makeText(getApplicationContext(),
+                            "Password too short, enter minimum 8 characters!",
+                                Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                //Creating User
+                mRef.createUserWithEmailAndPassword(email, password).addOnCompleteListener(GalmoreasRegPage.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Toast.makeText(GalmoreasRegPage.this, "createUserWithEmail:onComplete:" + task.isSuccessful(), Toast.LENGTH_SHORT).show();
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (!task.isSuccessful()) {
+                            Toast.makeText(GalmoreasRegPage.this, "Authentication failed." + task.getException(),
+                                    Toast.LENGTH_SHORT).show();
+                        } else {
+                            startActivity(new Intent(GalmoreasRegPage.this, GalmoreasLoginPage.class));
+                            finish();
+                        }
+
+                    }
+                });
 
             }
-
-
         });
+
 
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
 
 }
