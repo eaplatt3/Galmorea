ceac532c6354529bfa0615ab0e901461bd5ecf2f
Earl Platt III <eaplatt3@gmail.com>
2019-01-16 14:34:39 -0500

    More work on Google+ sign in

--- app/src/main/java/com/example/sickl/galmorea/GalmoreasLoginPage.java
+++ app/src/main/java/com/example/sickl/galmorea/GalmoreasLoginPage.java
@@ -1,22 +1,32 @@
 package com.example.sickl.galmorea;
 
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.graphics.drawable.AnimationDrawable;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
@@ -25,7 +35,7 @@ import com.google.firebase.auth.FirebaseAuth;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class GalmoreasLoginPage extends AppCompatActivity {
+public class GalmoreasLoginPage extends AppCompatActivity implements View.OnClickListener, GoogleApiClient.OnConnectionFailedListener {
 
     @BindView(R.id.imageView)
     ImageView login_idle;
@@ -39,8 +49,21 @@ public class GalmoreasLoginPage extends AppCompatActivity {
     Button loginBtn;
     @BindView(R.id.passwd_reset)
     TextView password_reset;
-    private GoogleSignInClient mGoogleSignInClient;
+    @BindView(R.id.sign_in_button)
+    SignInButton btnSignin;
+    @BindView(R.id.llProfile)
+    LinearLayout llProfileLayout;
+    @BindView(R.id.imgProfilePic)
+    ImageView imgProfilePic;
+    @BindView(R.id.txtName)
+    TextView txtName;
+    @BindView(R.id.txtEmail)
+    TextView txtEmail;
     private FirebaseAuth auth;
+    private static final String TAG = GalmoreasLoginPage.class.getSimpleName();
+    private static final int RC_SIGN_IN = 007;
+    private GoogleApiClient mGoogleApiClient;
+    private ProgressDialog mProgressDialog;
 
 
     @Override
@@ -55,6 +78,12 @@ public class GalmoreasLoginPage extends AppCompatActivity {
 
         //Get Authentication from Firebase
         auth = FirebaseAuth.getInstance();
+
+        btnSignin.setOnClickListener(this);
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
 
         if (auth.getCurrentUser() != null) {
             startActivity(new Intent(GalmoreasLoginPage.this, GalmoreasGameStart.class));
@@ -123,5 +152,49 @@ public class GalmoreasLoginPage extends AppCompatActivity {
 
             }
         });
+    }
+
+    private void signIn(){
+        Intent signInIntent = Auth.GoogleSignInApi(mGoogleApiClient);
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    private void signOut(){
+        Auth.GoogleSignInApi.signOut(mGoogleApiClient).setResultCallback(new ResultCallback<Status>() {
+            @Override
+            public void onResult(@NonNull Status status) {
+                updateUI(false);
+            }
+        });
+    }
+
+    private void handleSignInResult(GoogleSignInResult result){
+        Log.d(TAG, "handleSignInResult:" + result.isSuccess());
+        if(result.isSuccess()){
+            //Signed in successfully, show authenticated UI.
+            GoogleSignInAccount acct = result.getSignInAccount();
+            Log.e(TAG, "display name: " + acct.getDisplayName());
+
+            String personName = acct.getDisplayName();
+            String personPhotoUrl = acct.getPhotoUrl().toString();
+            String email = acct.getEmail();
+
+            Log.e(TAG, "Name: " + personName + ", email: " + email
+                    + ", Image: " + personPhotoUrl);
+
+            txtName.setText(personName);
+            txtEmail.setText(email);
+            Glide.with(getApplicationContext()).load(personPhotoUrl)
+                    .thumbnail(0.5f)
+                    .crossFade()
+                    .diskCacheStrategy(DiskCacheStrategy.ALL)
+                    .into(imgProfilePic);
+
+            updateUI(true);
+        } else {
+            // Signed out, show unauthenticated UI.
+            updateUI(false);
+        }
+
     }
 }
