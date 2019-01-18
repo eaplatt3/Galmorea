e20ddb75afe38cd9770e7ee3551b2f7bb1dde646
Earl Platt III <eaplatt3@gmail.com>
2019-01-18 10:02:31 -0500

    Added password reset page

---
+++ app/src/main/java/com/example/sickl/galmorea/GalmoreasPasswordReset.java
@@ -0,0 +1,13 @@
+package com.example.sickl.galmorea;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GalmoreasPasswordReset extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_galmoreas_password_reset);
+    }
+}
