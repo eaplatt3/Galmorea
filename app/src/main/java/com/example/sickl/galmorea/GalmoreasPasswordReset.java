c1a4a69a63a922d2de31f71a37dfc8209932f331
Earl Platt III <eaplatt3@gmail.com>
2019-01-28 14:46:43 -0500

    Added set onClick to return user back to Login page

--- app/src/main/java/com/example/sickl/galmorea/GalmoreasPasswordReset.java
+++ app/src/main/java/com/example/sickl/galmorea/GalmoreasPasswordReset.java
@@ -6,14 +6,39 @@
 
 package com.example.sickl.galmorea;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class GalmoreasPasswordReset extends AppCompatActivity {
+
+    @BindView(R.id.email_box)
+    EditText emailBox;
+    @BindView(R.id.rst_btn)
+    Button reSetBtn;
+    @BindView(R.id.bck_button)
+    TextView bckBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_galmoreas_password_reset);
+        ButterKnife.bind(this);
+
+        //Returns User to Login Page When Clicking Back Button
+        bckBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(GalmoreasPasswordReset.this, GalmoreasLoginPage.class));
+                finish();
+            }
+        });
     }
 }
